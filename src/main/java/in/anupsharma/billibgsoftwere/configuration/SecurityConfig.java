86de41b70bc4d07bb1799909d7385f0d3cc2b3cf
Anup sharma <132664325+Anup00007@users.noreply.github.com>
2025-07-22 02:38:15 +0530

    Update SecurityConfig.java

--- src/main/java/in/anupsharma/billibgsoftwere/configuration/SecurityConfig.java
+++ src/main/java/in/anupsharma/billibgsoftwere/configuration/SecurityConfig.java
@@ -19,7 +19,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -44,7 +45,7 @@ public class SecurityConfig {
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOriginPattern(frontendUrl);
+        config.addAllowedOriginPattern(List.of(frontendUrl));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization","Content-Type"));
         config.setAllowCredentials(true);
