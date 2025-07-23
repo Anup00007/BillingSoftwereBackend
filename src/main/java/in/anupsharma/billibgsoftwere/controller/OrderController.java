279d0d1d646608fee46374bd2af0f8736bc497a1
Anup sharma <132664325+Anup00007@users.noreply.github.com>
2025-07-23 05:48:44 +0530

    Update OrderController.java

--- src/main/java/in/anupsharma/billibgsoftwere/controller/OrderController.java
+++ src/main/java/in/anupsharma/billibgsoftwere/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("admin/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
